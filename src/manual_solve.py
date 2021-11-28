609a31676364bb41842d3808324b8986e53ef9f6
Cesar Alba <cesar.alba.moreno@gmail.com>
2021-11-28 16:36:02 +0000

    Added solution for task solve_6e19193c

--- src/manual_solve.py
+++ src/manual_solve.py
@@ -42,6 +42,82 @@ def solve_b94a9452(x):
     x=np.where(x == colors[0], colors[1], colors[0])
     return x
 
+def solve_6e19193c(x):
+    """
+    Description:
+        The expected input for this task is a grid with at least 1 sequence of cells that generates a L shape(3 cells), 
+        all of them have the same color.
+        This shape could come in four rotations: 0,90,180,270 degrees.
+        
+        The transformation is a copy of the original grid(same size and the L's i the same position) but adding a diagonal line
+        line for each L shape. The diagonals will be projected until the limit of the grid depending on the rotation in the following directions:
+            0 degrees: Diagonal projected to the right top 
+            90 degrees: Diagonal projected to the left top 
+            180 degrees: Diagonal projected to the left bottom 
+            270 degrees: Diagonal projected to the right bottom 
+        The origin of the digonal line is the outer cell of the L shape realtive to the intersection.
+    
+    """
+    # Get the general information of the grid : size and color of the L shapes
+    # Store the size of the grid in two variables w(width) and h(height)
+    w, h = x.shape
+    # Get the different colors of the grid removing the blank color(0) to get the color of the shapes
+    color= np.delete(np.unique(x), 0)[0]
+    
+    #Array to store all the x,y pairs that represents the origin of the diagonal lines
+    origins=[]
+    
+    for j in range(w):
+        for i in range(h):
+            #Finds 2 consecutive filled cell in the x axis.There is an L shape
+            if x[i][j]== color and j+1 <= w and x[i][j+1]== color:
+                #Find the degrees of the shape
+                #validate if it's an L 0 degrees
+                if i - 1 >= 0 and  x[i-1][j]== color:
+                    origins.append((0, i-1, j+1))
+                #Validate if it's an L 90 degrees
+                if i - 1 >= 0 and  x[i-1][j+1]== color:
+                    origins.append((90, i-1, j))
+                #Validate if it's an L 180 degrees
+                if i + 1 <= h and  x[i+1][j+1]== color:
+                    origins.append((180, i+1, j))
+                #Validate if it's an L 270 degrees
+                if i + 1 <= h and  x[i+1][j]== color:
+                    origins.append((270, i+1, j+1))
+                
+    #For each origin draw a line
+    # Depending on the direction of the line the limits must be checked to avoid out of bund errors
+    for origin in origins:
+        #0 degrees
+        if origin[0]==0 and origin[1] - 1 >=0 and origin[2]+1 <= w :
+            col_index=origin[2]+1;
+            for i in range(origin[1] - 1, -1, -1):
+                if(col_index <= w-1):
+                    x[i][col_index]=color
+                    col_index+=1
+        #90 degrees
+        if origin[0]==90 and origin[1] - 1 >=0 and origin[2]-1 >= 0 :
+            col_index=origin[2]-1
+            for i in range(origin[1] - 1, -1, -1):
+                if(col_index >= 0):
+                    x[i][col_index]=color
+                    col_index-=1
+        #180 degrees
+        if origin[0]==180 and origin[1] + 1 <=h and origin[2]-1 >= 0 :
+            col_index=origin[2]-1
+            for i in range(origin[1] + 1, h, 1):
+                if(col_index >= 0):
+                    x[i][col_index]=color
+                    col_index-=1
+        #270 degrees
+        if origin[0]==270 and origin[1] + 1 <=h and origin[2]+1 <= w :
+            col_index=origin[2]+1
+            for i in range(origin[1] + 1, h, 1):
+                if(col_index <= w-1):
+                    x[i][col_index]=color
+                    col_index+=1
+    return x
+
 def main():
     # Find all the functions defined in this file whose names are
     # like solve_abcd1234(), and run them.
